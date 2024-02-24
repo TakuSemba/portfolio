2acd831ebb3a8058f863bf639edd7131206f2a2e
takusemba <takusemba.ele@gmail.com>
2024-02-24 09:48:25 +0900

    update script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,8 +5,8 @@
 export FIREBASE_TOKEN=1//0ed0Z6OPWBI71CgYIARAAGA4SNwF-L9IrvNrexNvxmQjr0SjRu6L-x01PMgan7RzmxsMHk8rr2nWaifXgGzwOjankXZm2lX9RmpQ
 
 # build
-wget https://github.com/gohugoio/hugo/releases/download/v0.123.3/hugo_0.123.3_linux-arm64.deb
-sudo dpkg -i hugo_0.123.3_linux-arm64.deb
+wget https://github.com/gohugoio/hugo/releases/download/v0.123.3/hugo_0.123.3_linux-amd64.deb
+sudo dpkg -i hugo_0.123.3_linux-amd64.deb
 hugo
 
 # deploy
