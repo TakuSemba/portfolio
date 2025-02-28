21771f855efd6f1e762eeec77d4fe33367da235b
takusemba <takusemba.ele@gmail.com>
2025-02-28 22:13:15 +0900

    feat: update script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-wget https://github.com/gohugoio/hugo/releases/download/v0.123.3/hugo_0.123.3_linux-amd64.deb
-sudo dpkg -i hugo_0.123.3_linux-amd64.deb
+CGO_ENABLED=1 go install -tags extended github.com/gohugoio/hugo@v0.123.3
 hugo
